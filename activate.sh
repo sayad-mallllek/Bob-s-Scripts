62afa2f10f9d9ef8dff1a1b3e6a0f174e27a89d1
sayad-malllek <bob@khalilelzein.com>
2022-09-23 00:38:27 +0300

    finished activate script

--- activate.sh
+++ activate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cp ./creacreate-react-typescript-file-with-scss/create.sh /usr/bin/tsxcreate.sh
-cp ./create-nest-component/create.sh /user/bin/nestcreate.sh
-printf "alias tsxcreate='/usr/bin/tsxcreate.sh'" | cat >> ~/.bashrc
-printf "alias nestcreate='/usr/bin/nestcreate.sh'" | cat >> ~/.bashrc
+mkdir -p $HOME/scripts
+cp ./create-react-typescript-file-with-scss/create.sh $HOME/scripts/tsxcreate.sh || exit 1
+cp ./create-nest-component/create.sh $HOME/scripts/nestcreate.sh || exit 1
+echo -e "alias tsxcreate='bash $HOME/scripts/tsxcreate.sh'" >> "$HOME/.bashrc" || exit 1
+echo -e "alias nestcreate='bash $HOME/scripts/nestcreate.sh'" >> "$HOME/.bashrc" || exit 1
+source "$HOME/.bashrc" || exit 1
