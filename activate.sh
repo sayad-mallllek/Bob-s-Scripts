69120504573b33b789006b95349d6be7b26f5912
sayad-malllek <bob@khalilelzein.com>
2022-09-29 00:24:21 +0300

    fix: activate.sh unfinished implementation of api route

--- activate.sh
+++ activate.sh
@@ -6,5 +6,5 @@ cp ./create-nest-component/create.sh $HOME/scripts/nestcreate.sh || exit 1
 cp ./create-react-api-client/create.sh $HOME/scripts/react_apicreate.sh || exit 1
 echo -e "alias tsxcreate='bash $HOME/scripts/tsxcreate.sh'" >> "$HOME/.bashrc" || exit 1
 echo -e "alias nestcreate='bash $HOME/scripts/nestcreate.sh'" >> "$HOME/.bashrc" || exit 1
-echo -e "alias react_apicreate='bash $HOME/scripts/react_apicreate.sh'" >> "$HOME/.bashrc" || exit 1
+echo -e "alias react_api_init='bash $HOME/scripts/react_apicreate.sh'" >> "$HOME/.bashrc" || exit 1
 source "$HOME/.bashrc" || exit 1
---
+++ create-react-api-route/create.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Initial Values
+api_route_prefix="src/api/"
+api_route="/"
+folder_name=""
+file_name=""
+api_method="get"
+
+# Functions
+get_help() {
+    printf "react_api_create allows you to create your react api route\n"
+}
+
+while getopts ":r:m:f:h" flag;
+do
+    case "$flag" in
+        r) api_route=${OPTARG};;
+        m) api_method=${OPTARG};;
+        f) folder_name=${OPTARG};;
+        n) file_name=${OPTARG};;
+        h) get_help
+            exit 0;;
+        \?) printf "flag: ${OPTARG} is not a valid flag\n"
+            exit 1
+            ;;
+    esac
+done
+
+full_route="${api_route_prefix}${file_name}"
+
+if [ ! -z $folder_name ]; then
+     full_route="${api_route_prefix}${folder_name}/${file_name}"
+fi
+
+# echo "Username: $username";
+# echo "Age: $age";
+# echo "Full Name: $fullname";
