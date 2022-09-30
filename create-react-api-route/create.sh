2d9655f7e748f03eda4b50cdaf6befef737c3d23
sayad-malllek <bob@khalilelzein.com>
2022-09-30 08:36:41 +0300

    unfinished implem

--- create-react-api-route/create.sh
+++ create-react-api-route/create.sh
@@ -12,23 +12,41 @@ get_help() {
     printf "react_api_create allows you to create your react api route\n"
 }
 
-# Script Execution
+# Flags assignment
 
-if [ $# -eq 0 ]; then
-    printf "$# arguments were provided\nYou need to specify 2 arguments [file_name] [a"
+while getopts ":h" flag; do
+    case "$flag" in
+    h) get_help
+        exit 0
+        ;;
+    \?) ;;
+    esac
+done
+
+# Validations
+
+if [ ! $# -eq 2 ]; then
+    printf "%d arguments were provided\nYou need to specify 2 arguments [file_name] [api_route]\n" "$#"
     exit 1
 fi
 
-echo "$1"
+if ! [[ $1 =~ ^[a-zA-Z0-9]+$ ]]; then
+    printf "Invalid api method and file name! Name must contain letters and numbers only!\n"
+    exit 1
+fi
 
-while getopts ":r:m:f:n:h" flag; do
+if ! [[ $2 =~ ^/[a-zA-Z0-9/_=?-]+$ ]]; then
+    printf "Invalid api route! Route must start with '/' and must contain letters, numbers, or either characters ['/','?','=','-','_'] only!\n"
+    exit 1
+fi
+
+# Script Execution
+
+while getopts ":m:f:h" flag; do
     case "$flag" in
-    r) api_route=${OPTARG} ;;
     m) api_method=${OPTARG} ;;
     f) folder_name=${OPTARG} ;;
-    n) file_name=${OPTARG} ;;
-    h)
-        get_help
+    h) get_help
         exit 0
         ;;
     \?)
@@ -38,16 +56,16 @@ while getopts ":r:m:f:n:h" flag; do
     esac
 done
 
-# full_route="${api_route_prefix}${file_name}"
+full_route="${api_route_prefix}${file_name}"
 
-# if [ ! -z $folder_name ]; then
-#     full_route="${api_route_prefix}${folder_name}/${file_name}"
-# fi
+if [ ! -z $folder_name ]; then
+    full_route="${api_route_prefix}${folder_name}/${file_name}"
+fi
 
-# full_pathname="${full_route}/${file_name}.ts"
+full_pathname="${full_route}/${file_name}.ts"
 
-# mkdir -p "$full_route"
-# touch "$full_pathname"
+mkdir -p "$full_route"
+touch "$full_pathname"
 # # echo "Username: $username";
 # # echo "Age: $age";
 # # echo "Full Name: $fullname";
